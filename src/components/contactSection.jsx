d54f2dd9f1fb179b5bff338d010e8b2835c82961
Samuk <marcelolamas19@gmail.com>
2023-10-20 00:09:57 -0300

    Add props for each input of the contact section

--- src/components/contactSection.jsx
+++ src/components/contactSection.jsx
@@ -3,37 +3,54 @@ import {Input} from './inputs.jsx'
 
 import '../assets/styles/sections.css'
 
-function Contact() {
+function Contact({value,set}) {
     
 
     return (
 
-        <>
+        <form>
 
             <h2>Contact</h2>
 
             <Input 
                     label={'Website'}
                     placeholder={'www.yuuji.com'}
+                    name={'contact'}
+                    value={value.contact.website}
+                    handleSetText={set}
+                    fieldName={'website'}
             />
             <Input 
                     label={'Email'}
                     placeholder={'yuuji@gmail.com'}
+                    name={'contact'}
+                    value={value.contact.email}
+                    handleSetText={set}
+                    fieldName={'email'}
             />
 
             <div className='locationAndPhone style-twoInputs'>
                 <Input 
                     label={'Location'}
                     placeholder={'Los nashes 122'}
+                    name={'contact'}
+                    value={value.contact.location}
+                    handleSetText={set}
+                    fieldName={'location'}
                 />
                 <Input 
                     label={'Phone'}
                     placeholder={'555-555-555'}
+                    name={'contact'}
+                    value={value.contact.phone}
+                    handleSetText={set}
+                    fieldName={'phone'}
+                    type='tel'
                 />
             </div>   
 
             
-        </>
+        </form>
 
     )
 }
