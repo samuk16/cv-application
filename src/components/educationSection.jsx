86e8224711828b5c6cbb76f53b4fb96f9ec5be15
Samuk <marcelolamas19@gmail.com>
2023-10-20 00:24:36 -0300

    Add props for each input of the education section

--- src/components/educationSection.jsx
+++ src/components/educationSection.jsx
@@ -3,42 +3,67 @@ import { Controls } from './controls.jsx'
 import addSvg from '../assets/images/add.svg'
 import '../assets/styles/sections.css'
 
-function Education() {
+function Education({value,set}) {
     
 
     return (
 
         <>
 
-            <h2>Education</h2>
-
-            <div className='style-twoInputs'>
-                <Input 
-                    label={'Degree'}
-                    
-                />
-                <Input 
-                    label={'University'}
-                />
-            </div>
-
-            <div className='style-twoInputs'>
-                <Input 
-                    label={'From'}
-                />
-                <Input 
-                    label={'To'}
-                />
-            </div>
-
-
+            <form>
+        
+                <h2>Education</h2>
+        
+                <div className='style-twoInputs'>
+                    <Input 
+                        label={'Degree'}
+                        placeholder={'Bachelor of Science'}
+                        name={'education'}
+                        value={value.education.degree}
+                        handleSetText={set}
+                        fieldName={'degree'}
+                        
+                    />
+                    <Input 
+                        label={'University'}
+                        placeholder={'Stanford University'}
+                        name={'education'}
+                        value={value.education.university}
+                        handleSetText={set}
+                        fieldName={'university'}
+                    />
+                </div>
+        
+                <div className='style-twoInputs'>
+                    <Input 
+                        label={'From'}
+                        placeholder={'2019'}
+                        name={'education'}
+                        value={value.education.from}
+                        handleSetText={set}
+                        fieldName={'from'}
+                        type='number'
+                    />
+                    <Input 
+                        label={'To'}
+                        placeholder={'2023'}
+                        name={'education'}
+                        value={value.education.to}
+                        handleSetText={set}
+                        fieldName={'to'}
+                        type='number'
+                    />
+                </div>
+        
+        
+            </form>
 
             <Controls 
                 svgRight={addSvg}
             />                   
 
             
-        </>
+    </>
 
     )
 }
