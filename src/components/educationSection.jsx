e96e3086590ac4be80667a77a5925338e73e5c44
Samuk <marcelolamas19@gmail.com>
2023-10-23 23:25:19 -0300

    Add secondary component and ternary operator to render a btn or controls

--- src/components/educationSection.jsx
+++ src/components/educationSection.jsx
@@ -1,71 +1,97 @@
 import {Input} from './inputs.jsx'
 import { Controls } from './controls.jsx'
 import addSvg from '../assets/images/add.svg'
+import { Button } from './buttons.jsx'
 import '../assets/styles/sections.css'
 
-function Education({value,set}) {
-    
-
+function FormEducation({value,set,index}) {
     return (
 
         <>
 
             <form>
-        
+
                 <h2>Education</h2>
-        
+
                 <div className='style-twoInputs'>
                     <Input 
                         label={'Degree'}
                         placeholder={'Bachelor of Science'}
                         name={'education'}
-                        value={value.education.degree}
+                        value={value.education[index].degree}
                         handleSetText={set}
                         fieldName={'degree'}
+                        id={index}
                         
+
                     />
                     <Input 
                         label={'University'}
                         placeholder={'Stanford University'}
                         name={'education'}
-                        value={value.education.university}
+                        value={value.education[index].university}
                         handleSetText={set}
                         fieldName={'university'}
+                        id={index}
+
                     />
                 </div>
-        
+
                 <div className='style-twoInputs'>
                     <Input 
                         label={'From'}
                         placeholder={'2019'}
                         name={'education'}
-                        value={value.education.from}
+                        value={value.education[index].from}
                         handleSetText={set}
                         fieldName={'from'}
                         type='number'
+                        id={index}
+
                     />
                     <Input 
                         label={'To'}
                         placeholder={'2023'}
                         name={'education'}
-                        value={value.education.to}
+                        value={value.education[index].to}
                         handleSetText={set}
                         fieldName={'to'}
                         type='number'
+                        id={index}
+
                     />
                 </div>
-        
-        
+
+
             </form>
 
-            <Controls 
-                svgRight={addSvg}
-            />                   
+                               
 
             
-    </>
+        </>
 
     )
+}
+
+function Education({value,set,index,setIndex,data,onClick}) {
+
+    return(
+        <>
+            <FormEducation 
+                value={value}
+                set={set}
+                index={index}
+            />
+    
+            {
+                
+                data.length > 1 ? <Controls svgRight={addSvg} index={index} setIndex={setIndex} data={data} /> : <Button svg={addSvg} handleClick={onClick} section={'education'}/>
+               
+            }
+            
+        </>
+    )
+   
 }
 
 export {Education}
